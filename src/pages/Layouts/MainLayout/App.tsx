0782a08ccb43ec00845d3a42b480195dcc6bd86e
Parsa Bordbar <parsab71@gmail.com>
2023-06-28 23:23:47 +0330

    v1.1

--- src/pages/Layouts/MainLayout/App.tsx
+++ src/pages/Layouts/MainLayout/App.tsx
@@ -1,11 +1,19 @@
-import { Nav } from "../../../Components/Navbar";
+import { Nav,NavItem } from "../../../Components/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../../../Components/Footer";
 
 function Layout(){
   return (
     <>
-    <Nav/>
+    <Nav>
+      <NavItem tittle="Home" linkTo="/"/>
+      <NavItem tittle="Phones" linkTo="/phone"/>
+      <NavItem tittle="SMT" linkTo="/phone"/>
+      <NavItem tittle="Keyboards" linkTo="/phone"/>
+      <NavItem tittle="Monitors" linkTo="/phone"/>
+      <NavItem tittle="Pc" linkTo="/phone"/>
+      <NavItem tittle="Deals" linkTo="/phone"/>
+    </Nav>
     <Outlet />
     <Footer/>
     </>
